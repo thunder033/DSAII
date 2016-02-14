2d724a4aa4849b3e2a902b1b293114ad891357a5
Greg Rozmarynowycz <crashtest1@live.com>
2016-02-14 13:21:35 -0500

    add comments, clean up code

--- A03_Templated_List/Main.cpp
+++ A03_Templated_List/Main.cpp
@@ -4,34 +4,68 @@ using namespace std;
 
 void main(void) {
 
+	//Test Basic Constructor
+	cout << "Create TemplateList<int>" << endl;
 	TemplatedList<int> intList = TemplatedList<int>();
+	//Push adds one item: entries = 1, size = 1
+	cout << "Push int 3" << endl;
 	intList.Push(3);
+	//Push adds 2nd item, expands memory: entries = 2, size = 2
+	cout << "Push int 4" << endl;
 	intList.Push(4);
+	//Push adds 3rd item, expands memory: entries = 3, size = 4
+	cout << "Push int 2" << endl;
 	intList.Push(2);
+	cout << endl;
 
-	intList[1] = 22;
-	cout << "Int List item 1: " << intList[1] << endl;
+	//usage of GetSize
+	cout << "size before pop: " << intList.GetSize() << endl;
+	//usage of Pop, removes and returns last item
+	cout << "pop on intList:" << intList.Pop() << endl;
+	//Check size again, output popped item
+	cout << "size after pop: " << intList.GetSize() << endl;
+	cout << endl;
+
+	//Usage of operator[] overload
+	cout << "Set intList[1] to 22..." << endl;
+	intList[1] = 22; //setter
+	cout << "Expecting intList[1] = 22" << endl;
+	cout << "Int List item 1: " << intList[1] << endl; //getter
+	cout << endl;
+
+	//Print out list
 	intList.Print();
+	cout << endl;
 
-	cout << intList.GetSize() << endl;
-	int myInt = intList.Pop();
-	cout << intList.GetSize() << endl;
-	intList.Push(5);
-	cout << myInt << endl;
-
+	//Test Copy Ctor Overload
+	cout << "create 2nd list with copy constructor" << endl;
 	TemplatedList<int> intList2(intList);
+
+	//Test Copy Assignemnt overload
+	cout << "create 3rd list" << endl;
 	TemplatedList<int> intList3 = TemplatedList<int>();
+	cout << "add an item" << endl;
 	intList3.Push(10);
+	cout << "use copy assignment operator" << endl;
 	intList2 = intList3;
+	cout << endl;
 
+	//Usage of IsEmpty()
+	cout << "Using is empty" << endl;
 	cout << (intList3.IsEmpty() ? "List 3 has items" : "List 3 is empty") << endl;
-	intList3.Pop();
+	cout << "pop from intList3:" << intList3.Pop() << endl;
 	cout << (intList3.IsEmpty() ? "List 3 has items" : "List 3 is empty") << endl;
+	cout << endl;
 
+	//print out contents of all 3 lists to verify no memory issues
+	cout << "Verify list contents:" << endl;
 	cout << intList << endl;
 	cout << intList2 << endl;
 	cout << intList3 << endl;
+	cout << endl;
 
+	//clear contents of all 3 lists
+	cout << "Deallocate all lists" << endl;
 	intList.~TemplatedList();
 	intList2.~TemplatedList();
 	intList3.~TemplatedList();
--- A03_Templated_List/TemplatedList.h
+++ A03_Templated_List/TemplatedList.h
@@ -6,9 +6,14 @@ using namespace std;
 template<class T>
 class TemplatedList
 {
-	T* data;
-	int entries;
+	//number of items memory is allocated for
 	int size;
+	//number of items stored
+	int entries;
+	//pointer to data
+	T* data;
+
+	//free memory for data
 	void Release()
 	{
 		if (data != nullptr) {
@@ -16,8 +21,10 @@ class TemplatedList
 			data = nullptr;
 		}
 		entries = 0;
+		size = 1;
 	};
 
+	//swap two templated lists
 	void Swap(TemplatedList& other) {
 		std::swap(entries, other.entries);
 		std::swap(size, other.size);
@@ -25,39 +32,53 @@ class TemplatedList
 	};
 public:
 
+	//get number of items in list
 	int GetSize()
 	{
 		return entries;
 	};
 
+	//add new item to list
 	void Push(T item)
 	{
+		//if the allocated memory is full
 		if (size < entries + 1) {
-			//T* tempData = static_cast<T*>(malloc(size * 2 * sizeof(T)));
+			//create a temp pointer and allocate more memory
 			T* tempData = new T[size * 2];
+			//copy the existing data into the new memory
 			//memcpy(tempData, data, size * sizeof(T));
 			copy(data, data + size, tempData);
+			//deallocate old memory
 			delete[] data;
+			//point to new data
 			data = tempData;
+			//save the new size
 			size *= 2;
 		}
 
+		//add item at the next available location
 		*(data + entries++) = item;
 
 	};
 
+	//get the last item and remove it from the list
 	T Pop()
 	{
 		return *(data + --entries);
 	};
+
+	//indicates if there are any items in the list
 	bool IsEmpty()
 	{
 		return entries > 0;
 	};
+
+	//print out the list to the console
 	void Print() {
 		cout << *this;
 	}
 
+	//create stream containing size of list and contents
 	friend ostream& operator<<(ostream& os, const TemplatedList& list)
 	{
 		os << list.entries << " items in list: " << endl;
@@ -68,6 +89,7 @@ public:
 		return os;
 	};
 
+	//index operator overrides
 	T operator [](int i) const
 	{
 		return *(data + i);
@@ -79,20 +101,24 @@ public:
 	}
 
 	//Constructors
-	TemplatedList<T>() : data(nullptr), entries(0) 
+	//default constructor for simple initialization
+	TemplatedList<T>() 
+		: size(1),
+		entries(0),
+		data(new T[size])
+	{};
+
+	//constructor the allocates memory for specified number of items
+	TemplatedList<T>(int _entries) 
+		: size(1), 
+		entries(_entries), 
+		data(nullptr)
 	{
-		size = 1;
-		data = new T[size];
-	};
-	TemplatedList<T>(int _entries) : data(nullptr)
-	{
-		entries = _entries;
-		size = 1;
+		//figure out how much memory to allocate (next power of 2, i.e. 5 => 8, 30 => 32)
 		while (size < _entries) {
-			size = size * 2;
+			size *= 2;
 		}
 		data = new T[size];
-		//data = static_cast<T*>(malloc(tempSize * sizeof(T)));
 	};
 
 	//Destructor
@@ -103,21 +129,24 @@ public:
 
 	//Copy Constructor
 	TemplatedList(const TemplatedList<T>& other) 
-		: data(new T[other.size]),
-		size(other.size),
-		entries(other.entries) 
+		: size(other.size),
+		entries(other.entries),
+		data(new T[other.size])
 	{
-		cout << "Called Copy Constructor" << endl;
-		TemplatedList<T> temp(other.entries);
-		//memcpy(&temp.data, &other.data, other.size * sizeof(T));
-		copy(other.data, other.data + other.size, temp.data);
-		Swap(temp);
+		//copy other data into this new list
+		//TemplatedList<T> temp(other.entries);
+		//memcpy(temp.data, other.data, sizeof(T) * other.size);
+		//Swap(temp);
+
+		//there's not practical reason to use memcpy so just using std::copy
+		copy(other.data, other.data + other.size, data);
 	};
 
 	//Copy Assignment operator
+	//Pass by value executes copy ctor
 	TemplatedList& operator=(TemplatedList rhs)
 	{
-		cout << "Called Copy Assignment" << endl;
+		//don't need a self check here
 		Swap(rhs);
 		return *this;
 	};
